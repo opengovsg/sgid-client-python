a645a4818fb020bb096cda1a010fd2dec3f741b0
Antariksh <antarikshmahajan@gmail.com>
2023-05-07 12:59:58 +0800

    feat: implement authorization_url

--- sgid_client/SgidClient.py
+++ sgid_client/SgidClient.py
@@ -1,10 +1,13 @@
 import secrets
-from typing import TypedDict, Union
+from typing import TypedDict
+import urllib
+from urllib.parse import urlparse, urlencode
+from collections import namedtuple
 
 
 class AuthorizationUrlReturn(TypedDict):
     url: str
-    nonce: Union[str, None]
+    nonce: str | None
 
 
 class CallbackReturn(TypedDict):
@@ -23,7 +26,7 @@ class SgidClient:
         client_id: str,
         client_secret: str,
         private_key: str,
-        redirect_uri: str,
+        redirect_uri: str | None,
         hostname: str = "https://api.id.gov.sg",
         api_version: int = 1,
     ):
@@ -31,13 +34,36 @@ class SgidClient:
         self.client_secret = client_secret
         self.private_key = private_key
         self.redirect_uri = redirect_uri
-        self.hostname = hostname
+        self.hostname = urlparse(hostname).geturl()
         self.api_version = api_version
 
     def authorization_url(
-        self, state: str, nonce: str = secrets.token_urlsafe(16)
+        self,
+        state: str,
+        redirect_uri: str | None = None,
+        scope: str | list[str] = "openid myinfo.name",
+        nonce: str | None = secrets.token_urlsafe(32),
     ) -> AuthorizationUrlReturn:
-        pass
+        if redirect_uri is None and self.redirect_uri is None:
+            raise Exception(
+                "No redirect URI registered with this client. You must either specify a valid redirect URI in the SgidClient constructor, or pass it to the authorizationUrl and callback functions."
+            )
+        params = {
+            "client_id": self.client_id,
+            "scope": " ".join(scope) if isinstance(scope, list) else scope,
+            "redirect_uri": self.redirect_uri if redirect_uri is None else redirect_uri,
+            "response_type": "code",
+            "state": state,
+        }
+        if nonce is not None:
+            params["nonce"] = nonce
+        return (
+            self.hostname
+            + "/v"
+            + str(self.api_version)
+            + "/oauth/authorize?"
+            + urlencode(params)
+        )
 
     def callback(self, code: str, nonce: str = None) -> CallbackReturn:
         pass
