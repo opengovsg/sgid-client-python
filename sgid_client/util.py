b9c76a9cc441f932535a9b62d152584c156e1781
Antariksh <antarikshmahajan@gmail.com>
2023-05-19 17:01:42 +0800

    fix: add try-catch to convert_to_pkcs8

--- sgid_client/util.py
+++ sgid_client/util.py
@@ -1,6 +1,11 @@
 from Crypto.PublicKey import RSA
 
+from sgid_client.error import Errors
+
 
 def convert_to_pkcs8(private_key: str) -> str:
-    imported = RSA.import_key(extern_key=private_key)
-    return imported.export_key(pkcs=8).decode("ascii")
+    try:
+        imported = RSA.import_key(extern_key=private_key)
+        return imported.export_key(pkcs=8).decode("ascii")
+    except Exception as exc:
+        raise Exception(Errors["PRIVATE_KEY_IMPORT"]) from exc
